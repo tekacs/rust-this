49b4cbbd04d65fd5d94bdf834da64e825714daaf
Amar Sood (tekacs) <pkg@tekacs.com>
2017-02-09 21:09:28 +0000

    Version 0.2.0. Could be further styled.

---
+++ src/lib.rs
@@ -0,0 +1,38 @@
+use std::collections::HashMap;
+
+const S: &'static str =
+"Gur Mra bs Clguba, ol Gvz Crgref
+Ornhgvshy vf orggre guna htyl.
+Rkcyvpvg vf orggre guna vzcyvpvg.
+Fvzcyr vf orggre guna pbzcyrk.
+Pbzcyrk vf orggre guna pbzcyvpngrq.
+Syng vf orggre guna arfgrq.
+Fcnefr vf orggre guna qrafr.
+Ernqnovyvgl pbhagf.
+Fcrpvny pnfrf nera'g fcrpvny rabhtu gb oernx gur ehyrf.
+Nygubhtu cenpgvpnyvgl orngf chevgl.
+Reebef fubhyq arire cnff fvyragyl.
+Hayrff rkcyvpvgyl fvyraprq.
+Va gur snpr bs nzovthvgl, ershfr gur grzcgngvba gb thrff.
+Gurer fubhyq or bar-- naq cersrenoyl bayl bar --boivbhf jnl gb qb vg.
+Nygubhtu gung jnl znl abg or boivbhf ng svefg hayrff lbh'er Qhgpu.
+Abj vf orggre guna arire.
+Nygubhtu arire vf bsgra orggre guna *evtug* abj.
+Vs gur vzcyrzragngvba vf uneq gb rkcynva, vg'f n onq vqrn.
+Vs gur vzcyrzragngvba vf rnfl gb rkcynva, vg znl or n tbbq vqrn.
+Anzrfcnprf ner bar ubaxvat terng vqrn -- yrg'f qb zber bs gubfr!";
+
+pub fn this() -> String {
+    let mut d = HashMap::new();
+    for c in vec![65, 97] {
+        for i in 0..26 {
+            d.insert(char::from(i+c), char::from((i+13) % 26 + c));
+        }
+    }
+    S.chars().map(|ref c| *d.get(c).unwrap_or(c)).collect()
+}
+
+#[test]
+fn test_this() {
+    assert!(this().contains("Python"))
+}
